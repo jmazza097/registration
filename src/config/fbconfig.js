aecc6450208d21ee9095cb81b147ec80598a7be2
Jack Mazza <jmazza097@gmail.com>
2020-02-26 10:46:27 -0600

    added FB to project

---
+++ src/config/fbconfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCrAfQ8t1dC7GpkhKdiqHSnzPLI2yKY7rM",
+    authDomain: "registration-project-86414.firebaseapp.com",
+    databaseURL: "https://registration-project-86414.firebaseio.com",
+    projectId: "registration-project-86414",
+    storageBucket: "registration-project-86414.appspot.com",
+    messagingSenderId: "957846367264",
+    appId: "1:957846367264:web:92dba887a7b25240eb6045",
+    measurementId: "G-5JVK2LN4FV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore().settings({timestampsInSnapshots: true})
+
+  export default firebase;
