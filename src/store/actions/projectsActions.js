df4ec41ab0f20a6971722b7a0f3942be08b343e3
Jack Mazza <jmazza097@gmail.com>
2020-02-26 20:28:15 -0600

    lesson-17

--- src/store/actions/projectsActions.js
+++ src/store/actions/projectsActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) =>{
-    return(dispatch, getState) =>{
+    return(dispatch, getState, {getFirebase, getFireStore}) =>{
         //make async callto  db
         dispatch({type: 'CREATE_PROJECT', project: project})
     }
